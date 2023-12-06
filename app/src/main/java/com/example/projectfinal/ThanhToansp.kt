992b2a5ec8ddc540cd0a955139df64f3a348c2a7
Trieung <trieu0258@gmail.com>
2023-12-07 00:44:29 +0700

    thanh toan sp,quên mật khẩu,profile

---
+++ app/src/main/java/com/example/projectfinal/ThanhToansp.kt
@@ -0,0 +1,11 @@
+package com.example.projectfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThanhToansp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_thanh_toansp)
+    }
+}
